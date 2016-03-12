9b8c2034094213a60efa37b4dc953145ba7d6c77
Ryan Collins <rccproductions@mac.com>
2016-03-12 15:09:14 -0500

    Feat: begin to scaffold out routes for the entire site

--- routes/auth/service.js
+++ routes/auth/service.js
@@ -2,13 +2,16 @@ var keystone = require('keystone'),
 	async = require('async');
 
 var services = {
-	github: require('../../lib/auth/github')
+	github: require('../../lib/auth/github'),
+	twitter: require('../../lib/auth/twitter')
 };
 
 exports = module.exports = function(req, res, next) {
 
 	if (!req.params.service) {
-		console.log('[auth.service] - You must define the service you wish to authenticate with.');
+		console.log(
+			'[auth.service] - You must define the service you wish to authenticate with.'
+		);
 		return res.redirect('/signin');
 	}
 
