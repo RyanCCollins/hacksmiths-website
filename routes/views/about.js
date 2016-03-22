e3119a6c32c3e53bc23d05e4ee7afb0972f90cdb
Ryan Collins <rccproductions@mac.com>
2016-03-22 08:29:54 -0400

    Feat: add an about section to landing page

--- routes/views/about.js
+++ routes/views/about.js
@@ -12,12 +12,12 @@ exports = module.exports = function(req, res) {
 		name: 'Ryan Collins',
 		image: '/images/ryan.jpg',
 		twitter: 'ryancollinsio',
-		title: 'Udacity student extraordinaire.  Creator of Hacksmiths.'
+		title: 'Ryan built the Hacksmiths platform from the ground up.  He has nearly 8 years experience architecting large-scale web applications and data systems.  You can find him spending his free time contributing to open source projects and leading the Hacksmiths development team.'
 	}, {
 		name: 'Sean Craig',
 		image: '/images/sean.jpg',
 		twitter: 'swhc1066',
-		title: 'UI / UX Designer.'
+		title: 'Sean is a professional User Experience and Interface Designer with years of experience in the industry.  He runs the product design team here at the Hacksmiths project, donating his time to making his vision a reality.'
 	}]
 
 	view.render('site/about');
--- routes/views/index.js
+++ routes/views/index.js
@@ -1,15 +1,28 @@
 var keystone = require('keystone');
 
 exports = module.exports = function(req, res) {
-	
+
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
-	
+
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
 	locals.section = 'home';
-	
+	locals.title = 'Hacksmiths';
+
+	locals.organizers = [{
+		name: 'Ryan Collins',
+		image: '/images/ryan.jpg',
+		twitter: 'ryancollinsio',
+		title: 'Ryan built the Hacksmiths platform from the ground up.  He has nearly 8 years experience architecting large-scale web applications and data systems.  You can find him spending his free time contributing to open source projects and leading the Hacksmiths development team.'
+	}, {
+		name: 'Sean Craig',
+		image: '/images/sean.jpg',
+		twitter: 'swhc1066',
+		title: 'Sean is a professional User Experience and Interface Designer with years of experience in the industry.  He runs the product design team here at the Hacksmiths project, donating his time to making his vision a reality.'
+	}]
+
 	// Render the view
 	view.render('index');
-	
+
 };
