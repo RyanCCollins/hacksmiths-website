8173d58751e45ca404261f4837dfed8d6907915b
Ryan Collins <rccproductions@mac.com>
2016-03-25 14:06:24 -0400

    Feat: add mentoring page with a better explanation

--- models/Settings.js
+++ models/Settings.js
@@ -4,29 +4,17 @@ var modelToJSON = require('../lib/modelToJSON');
 var Types = keystone.Field.Types;
 
 var Settings = new keystone.List('Settings', {
-    nocreate: true,
-    nodelete: true
+  nocreate: true,
+  nodelete: true
 });
 
 Settings.add({
-    name: { type: String, noedit: true },
+  name: {
+    type: String,
+    noedit: true
+  },
 
-    showAboutView: { type: Boolean, default: false },
-    aboutButtonLink: { type: String, default: '' },
-    aboutButtonLabel: { type: String, default: '' },
-    aboutContent: { type: Types.Code, default: '' },
 
-    refreshInterval: { type: Number, default: 30000 },
-
-    eventPlaceName: { type: String },
-    eventAddress: { type: String },
-    eventMapsAddress: { type: String, note: 'Address for Maps, appended to http://maps.apple.com/?daddr=' },
-    eventTwitter: { type: String, size: 'small', note: 'Twitter Username (without @)' },
-    eventFacebook: { type: String, size: 'small', note: 'Facebook Username' },
-    eventFacebookPageId: { type: String, size: 'small', note: 'Derived PageID for Facebook, via https://app.urlgeni.us' },
-    registrationsOpen: { type: Boolean, default: true },
-
-    showResendEmail: { type: Boolean, default: false }
 });
 
 modelToJSON(Settings);
