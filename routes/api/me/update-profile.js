33f2e3b113d3e0ea3e02c17ce9b752e99c616e49
Ryan Collins <rccproductions@mac.com>
2016-04-11 23:52:52 -0400

    Fix: the update profile method

--- routes/api/me/update-profile.js
+++ routes/api/me/update-profile.js
@@ -1,22 +1,21 @@
-// var async = require('async'),
-// 	keystone = require('keystone'),
-// 	User = keystone.list('User');
-//
-// exports = module.exports = function(req, res) {
-// 	locals = {
-// 		user: {}
-// 	}
-//
-// 	User.findByID(req.body.user).exec(function(err, results))
-//
-// 	if (err) return;
-//
-// 	if (!user) return;:
-// 	for (var key in req.body.user) {
-// 		user[key] = req.body.user[key]
-// 	}
-//
-// 	person.save(function() {
-//
-// 	});
-// }
+var async = require('async'),
+	keystone = require('keystone'),
+	User = keystone.list('User');
+
+exports = module.exports = function(req, res) {
+	locals = {
+		user: {}
+	}
+
+	User.findByID(req.body.user).exec(function(err, results) {
+		if (err) return;
+		if (!user) return;
+		for (var key in req.body.user) {
+			user[key] = req.body.user[key];
+		}
+
+		person.save(function() {
+
+		});
+	});
+}
