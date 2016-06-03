7ddc6a741591a39a077de636315d4d7779f3850e
Ryan Collins <rccproductions@mac.com>
2016-06-03 10:11:01 -0400

    Feat: add a new method for creating auth hash

--- routes/api/app/signin-email.js
+++ routes/api/app/signin-email.js
@@ -1,5 +1,5 @@
 var keystone = require('keystone');
-
+var secrets = 'lib/auth/secrets';
 exports = module.exports = function(req, res) {
 
 	if (!req.body.username || !req.body.password) return res.apiResponse({ success: false });
@@ -13,13 +13,15 @@ exports = module.exports = function(req, res) {
 				message: (err && err.message ? err.message : false) || 'Sorry, there was an issue signing you in, please try again.'
 			});
 		}
-
+		var authToken = auth.hmac(user.id, secrets.tokenSecret);
 		keystone.session.signin({ email: user.email, password: req.body.password }, req, res, function(user) {
+			user.authHash = auth.hash(authToken);
 
 			return res.apiResponse({
 				success: true,
 				session: true,
 				date: new Date().getTime(),
+				authToken: authToken,
 				userId: user.id
 			});
 
